488de4045f94d9ed93d48a14060ed28da997cb26
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-06-04 13:32:09 -0400

    fucking what

---
+++ myproject/src/MyApp.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX is fully operational!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 250);
+        
+        primaryStage.setTitle("Setup Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
